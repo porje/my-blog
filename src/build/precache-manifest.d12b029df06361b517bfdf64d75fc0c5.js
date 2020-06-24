b8834715c7c9f855df3c7dba5d2a359c945d6392
Rahul Porje <as.rahul21@gmail.com>
2020-06-25 00:04:35 +0530

    initial commit

---
+++ src/build/precache-manifest.d12b029df06361b517bfdf64d75fc0c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4f39297f212ffccca43f86635fac9e3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3ee7d1915525ea2f03fa",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "00e78cd3834e93a789c7",
+    "url": "/static/js/2.8e70f688.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.8e70f688.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ee7d1915525ea2f03fa",
+    "url": "/static/js/main.72d03fbf.chunk.js"
+  },
+  {
+    "revision": "8931283fa7532b66ee7f",
+    "url": "/static/js/runtime-main.fca178c7.js"
+  }
+]);
